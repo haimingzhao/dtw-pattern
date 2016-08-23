ea096cad954f2e82fd5d515e528450e637adf9a8
Haiming Zhao <u1590812@warwick.ac.uk>
2016-08-23 21:40:32 +0100

    deleted unused file

--- src/MatrixKernel.cu
+++
@@ -1,9 +0,0 @@
-//
-// Created by Mira on 22/08/2016.
-//
-
-#include "MatrixKernel.h"
-
-#define getI_bl(i,j, nx, ny) ((nx-(i-j)-1) < ny) ? ( (nx-(i-j)-1)*(nx-(i-j))/2 + j ):( (ny-1)*(ny)/2 + (nx-(i-j)- ny)*ny + j ) ;
-#define getI_ur(i,j, nx, ny, uj0) (ny<=nx) ? (uj0+(ny+ny-j+i+1)*(j-i)/2+i) : ( (j-i<=ny-nx)?(uj0+nx*(j-i)+i):(uj0+nx*(ny-nx)+(nx+ny-j+i+1)*(j-i-ny+nx)/2+i) );
-
